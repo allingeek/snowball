80bcb6c3c0ee7f5285ea08a2f0b74fa6d3336851
Jeff Nickoloff <jeff@allingeek.com>
2014-12-13 22:08:30 +0000

    Removed some junk.

--- scripts/snowball.js
+++ scripts/snowball.js
@@ -1,19 +1,5 @@
 var snowball = angular.module('Snowball', ['firebase']);
 
-snowball.factory('', function() {
-  var service = {};
-  var running = true;
-
-  service.warmer = function() {
-    return running && tutorialQueue.length > 0
-  };
-  service.cooler = function() {
-    return running && tutorialQueue.length > 0
-  };
-
-  return service;
-});
-
 snowball.controller("TemperatureController", function($scope, $firebase) {
   var ref = new Firebase("https://popping-fire-4005.firebaseio.com/data");
   var sync = $firebase(ref);
